cec93d76ee5e444ba4e43f24f6354da769e734c0
deniisolo <deniisolo@outlook.es>
2024-08-11 14:34:14 -0500

    feat(ui, styles): first sight without information

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,23 +1,153 @@
+import React from "react";
+import image from "./pics/gato.jpeg";
+import logo from "./pics/logo.png";
+import nombre from "./pics/nombre1.png";
 
-import React from 'react';
+import { FaLinkedinIn } from "react-icons/fa";
 export function Home() {
-    return <div>
-     <div className="rigthside">
-     <img src="" alt="" />
-     <h1>Guardowl</h1>
-     <h3>Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas "Letraset", las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.</h3>
-     <button
-        type="button"
-        className="inline-block rounded bg-neutral-800 px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-neutral-50 shadow-[0_4px_9px_-4px_rgba(51,45,45,0.7)] transition duration-150 ease-in-out hover:bg-neutral-800 hover:shadow-[0_8px_9px_-4px_rgba(51,45,45,0.2),0_4px_18px_0_rgba(51,45,45,0.1)] focus:bg-neutral-800 focus:shadow-[0_8px_9px_-4px_rgba(51,45,45,0.2),0_4px_18px_0_rgba(51,45,45,0.1)] focus:outline-none focus:ring-0 active:bg-neutral-900 active:shadow-[0_8px_9px_-4px_rgba(51,45,45,0.2),0_4px_18px_0_rgba(51,45,45,0.1)] dark:bg-neutral-900 dark:shadow-[0_4px_9px_-4px_#030202] dark:hover:bg-neutral-900 dark:hover:shadow-[0_8px_9px_-4px_rgba(3,2,2,0.3),0_4px_18px_0_rgba(3,2,2,0.2)] dark:focus:bg-neutral-900 dark:focus:shadow-[0_8px_9px_-4px_rgba(3,2,2,0.3),0_4px_18px_0_rgba(3,2,2,0.2)] dark:active:bg-neutral-900 dark:active:shadow-[0_8px_9px_-4px_rgba(3,2,2,0.3),0_4px_18px_0_rgba(3,2,2,0.2)]"
-      >
-        Dark
-      </button>
-     <button >
-        Votar</button>
-     </div>
-     <div className="leftside">
-     <h1>Team</h1>
-     
-     </div>
+  return (
+    <div className="flex h-screen font-montserrat">
+      <div className="w-1/2 flex flex-col items-center justify-center gap-20 p-20">
+        <div className="flex flex-col items-center justify-center  gap-4">
+          <img src={logo} alt="" className="w-32 h-32 " />
+          <img src={nombre} alt="" />
+        </div>
+
+        <h3 className="text-white  font-montserrat">
+          Lorem Ipsum es simplemente el texto de relleno de las imprentas y
+          archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de
+          las industrias desde el año 1500, cuando un impresor (N. del T.
+          persona que se dedica a la imprenta) desconocido usó una galería de
+          textos y los mezcló de tal manera que logró hacer un libro de textos
+          especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como
+          texto de relleno en documentos electrónicos, quedando esencialmente
+          igual al original. Fue popularizado en los 60s con la creación de las
+          hojas "Letraset", las cuales contenian pasajes de Lorem Ipsum, y más
+          recientemente con software de autoedición, como por ejemplo Aldus
+          PageMaker, el cual incluye versiones de Lorem Ipsum.
+        </h3>
+        <button
+          type="button" 
+          class="bg-white font-montserrat text-gray-900 font-bold py-3 px-6 rounded-lg shadow-lg hover:bg-gray-100 hover:shadow-xl transition-all duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-105 border border-gray-300"
+        >
+          Votar
+        </button>
+      </div>  
+      <div className="w-1/2 flex flex-col  items-center justify-center gap-20 p-20">
+        <h1 className="text-4xl font-extrabold leading-none tracking-tight text-white ">Team</h1>
+        <div className="flex flex-wrap gap-10">
+          <div class="flex items-center gap-4">
+            <img class="w-14 h-14 rounded-full" src={image} alt="" />
+            <div class="font-medium text-white font-montserrat">
+              <div>Lesly Samaritano Ayala</div>
+              <div class="text-sm text-white-500">Jr Flutter Developer</div>
+
+              <a href="" class=" flex  text-sm text-white-500 gap-2 font-montserrat">
+                <FaLinkedinIn size={20} />
+                @gatitobonitoTQM
+              </a>
+            </div>
+          </div>
+          <div class="flex items-center gap-4 font-montserrat">
+            <img class="w-14 h-14 rounded-full" src={image} alt="" />
+            <div class="font-medium text-white font-montserrat">
+              <div>Gatito Bonito</div>
+              <div class="text-sm text-white-500 font-montserrat">Backend Developer</div>
+
+              <a href="" class=" flex  text-sm text-white-500 gap-2 font-montserrat">
+                <FaLinkedinIn size={20} />
+                @gatitobonitoTQM
+              </a>
+            </div>
+          </div>
+          <div class="flex items-center gap-4 font-montserrat">
+            <img class="w-14 h-14 rounded-full" src={image} alt="" />
+            <div class="font-medium text-white font-montserrat">
+              <div>Gatito Bonito</div>
+              <div class="text-sm text-white-500 font-montserrat">Backend Developer</div>
+
+              <a href="" class=" flex  text-sm text-white-500 gap-2 font-montserrat">
+                <FaLinkedinIn size={20} />
+                @gatitobonitoTQM
+              </a>
+            </div>
+          </div>
+          <div class="flex items-center gap-4 font-montserrat">
+            <img class="w-14 h-14 rounded-full" src={image} alt="" />
+            <div class="font-medium text-white font-montserrat">
+              <div>Gatito Bonito</div>
+              <div class="text-sm text-white-500 font-montserrat">Backend Developer</div>
+
+              <a href="" class=" flex  text-sm text-white-500 gap-2 font-montserrat">
+                <FaLinkedinIn size={20} />
+                @gatitobonitoTQM
+              </a>
+            </div>
+          </div>
+          <div class="flex items-center gap-4 font-montserrat">
+            <img class="w-14 h-14 rounded-full" src={image} alt="" />
+            <div class="font-medium text-white font-montserrat">
+              <div>Gatito Bonito</div>
+              <div class="text-sm text-white-500 font-montserrat">Backend Developer</div>
+
+              <a href="" class=" flex  text-sm text-white-500 gap-2 font-montserrat">
+                <FaLinkedinIn size={20} />
+                @gatitobonitoTQM
+              </a>
+            </div>
+          </div>
+          <div class="flex items-center gap-4 font-montserrat">
+            <img class="w-14 h-14 rounded-full" src={image} alt="" />
+            <div class="font-medium text-white font-montserrat">
+              <div>Gatito Bonito</div>
+              <div class="text-sm text-white-500 font-montserrat">Backend Developer</div>
+
+              <a href="" class=" flex  text-sm text-white-500 gap-2 font-montserrat">
+                <FaLinkedinIn size={20} />
+                @gatitobonitoTQM
+              </a>
+            </div>
+          </div>
+          <div class="flex items-center gap-4">
+            <img class="w-14 h-14 rounded-full" src={image} alt="" />
+            <div class="font-medium text-white">
+              <div>Gatito Bonito</div>
+              <div class="text-sm text-white-500">Backend Developer</div>
+
+              <a href="" class=" flex  text-sm text-white-500 gap-2">
+                <FaLinkedinIn size={20} />
+                @gatitobonitoTQM
+              </a>
+            </div>
+          </div>
+          <div class="flex items-center gap-4">
+            <img class="w-14 h-14 rounded-full" src={image} alt="" />
+            <div class="font-medium text-white">
+              <div>Gatito Bonito</div>
+              <div class="text-sm text-white-500">Backend Developer</div>
+
+              <a href="" class=" flex  text-sm text-white-500 gap-2">
+                <FaLinkedinIn size={20} />
+                @gatitobonitoTQM
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="area">
+			<ul class="circles">
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+			</ul>
+		</div>
     </div>
+  );
 }
